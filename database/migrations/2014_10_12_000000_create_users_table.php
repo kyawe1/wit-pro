500acb812fea4afed50f45dbb07dc2b359d1b6f2
kyawe1 <kk09254010314@gmail.com>
2020-12-22 20:42:15 +0630

    second commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('reader');
+            $table->string('profile-picture')->nullable();
+            $table->string('address')->nullable();
+            $table->string('work_at')->nullable();
+            $table->date('birthdate')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
